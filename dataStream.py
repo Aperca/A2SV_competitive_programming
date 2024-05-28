600059ca2a40dcfdc18c8eb71be520fc8deb1ed9
Adey <149935849+Aperca@users.noreply.github.com>
2024-05-28 19:07:27 +0300

    Update dataStream.py

--- dataStream.py
+++ dataStream.py
@@ -1,19 +1,25 @@
 class DataStream:
-
     def __init__(self, value: int, k: int):
         self.value = value
         self.k = k
         self.count = 0
-        self.last_k_elements = [None] * k  
+        self.last_k_elements = [None] * k
+        self.num_value_elements = 0
 
     def consec(self, num: int) -> bool:
-        self.last_k_elements[self.count % self.k] = num
-        self.count += 1
+        idx = self.count % self.k
+
+        if self.last_k_elements[idx] == self.value:
+            self.num_value_elements -= 1
+       
+        self.last_k_elements[idx] = num
         
+        if num == self.value:
+            self.num_value_elements += 1
+
+        self.count += 1
         if self.count < self.k:
             return False
-        
-        for i in range(self.k):
-            if self.last_k_elements[i] != self.value:
-                return False
-        return True
+
+        return self.num_value_elements == self.k
+
